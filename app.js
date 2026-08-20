94fada395d46ccedbf0532efd88f56d6aa0dd247
MOHDYAASAR <shaikmohammedyaasar07@gmail.com>
2026-08-20 10:43:07 +0530

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+const calc = (a,b)=>{
+    return a+b;
+}
+let a = calc(2,5);
+console.log(a);
+console.log("Result Calculated");
+
+let arr = [1,"name",2,3,4,5,8];
+for(let i = 0; i<arr.length; i++){
+    console.log(arr[i]+" ");
+}
